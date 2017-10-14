090e876a6c8be3f6bf4bc895a8c609a80a82065e
faker.yang <faker.yang@goertek.com>
2017-10-14 11:45:54 +0800

    读取txt数据并快速绘图

--- read_txt/draw_line.py
+++ read_txt/draw_line.py
@@ -13,7 +13,7 @@ for line in f:
 	if index > 2 and index != 10 and index != 12:
 		x = [0, 0.6, 1, 1.5, 14.5,15,16,17,19,19.5,20.5,21.5]
 		ylim(0,100)
-		plt.xlabel('time/minute')
+		plt.xlabel('time/hour')
 		plt.ylabel('temperature')
 		rs = [float(i) for i in rs]
 		#print(rs)
